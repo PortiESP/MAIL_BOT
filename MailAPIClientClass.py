ca9631dc67c4f117f7110cffca729856a1850536
Porti <elp0rti201@gmail.com>
2022-06-07 11:06:25 +0200

    Working complete v2.0 (not integrated)

--- MailAPIClientClass.py
+++ MailAPIClientClass.py
@@ -1,6 +1,4 @@
-from cv2 import add
 from MailAPI import MailAPI
-from random import randint
 import requests
 
 class MailCLI:
@@ -55,6 +53,9 @@ class MailCLI:
 
     def saveCache(self):
         
+        if self.disableCache:
+            return False
+
         with open(self.cachePath, 'w') as fd:
             for line in self.cacheAccounts:
                 fd.write(f"{line.creeds['address']},{line.creeds['password']},{line.account['id']},{line.token}\n")
@@ -138,10 +139,11 @@ class MailCLI:
         return accList
                 
     # Find address in object list
-    def findInCache(self, addr):
+    def findInCache(self, filter):
+        
         for i,cache in enumerate(self.cacheAccounts):
-            if addr in cache.creeds['address']:
-                return i
+            if filter in cache.creeds['address'] or filter in cache.account['id']:
+                return self.cacheAccounts[i]
         else:
             return None
 
@@ -158,7 +160,7 @@ class MailCLI:
                 data = account.split(":")
                 index = self.findInCache(data[0])
                 if index:
-                    account = self.cacheAccounts[index]
+                    account = index
                 else:
                     account = MailAPI()
                     account.login(email=data[0], password=data[1])
@@ -192,7 +194,100 @@ class MailCLI:
             """ % acc.creeds['address'])
             acc.printEmailsList(seen=showseen)
 
-    def mark(self):
+    def mark(self, eid=None, acc=None):
+        if eid:
+            acc.markAsSeen(eid)
+
+            return
+
         for acc in self.logged_in_accounts:
             for email in acc.getAllEmails():
                 acc.markAsSeen(email['id'])
+
+
+    def helpAdvanced(self):
+        print(f"""
+        
+        usage: {path.basename(sys.argv[0])} [-h] [-H] [-n <user>:<pass>] [-N <creeds.txt>] [-R <x>] [-d <account id>] [-D <creeds.txt>] [-l <user>:<pass>] [-L <creeds.txt>] [-i] [-I] [-m] [-o <creeds.txt>] [-q]
+
+        This is the CLI from the API from -> https://api.mail.tm
+
+        The main usage for this too is for manitoring multiple accounts simultaneously.
+
+        The API has limited the requests to 8 per second so we cant go any faster than that, this is why it is recommended to use cache for credentials,
+        this will prevent to make unnecesary requests to the API
+
+        Optional arguments:
+            Help:
+                -h, --help            Show default help menu
+                -H, --help-advanced   Show this help menu
+                -q, --quiet           The program wont output anything to the stdout
+
+            Output:
+                # This is where the credentials stored in cache will be stored, format: <address>:<pass>
+                -o <creeds.txt>, --output <creeds.txt>
+
+            Cache:
+                # Prevent the program to save cache data
+                --disable-cache
+
+        Account create & delete arguments:
+
+            Create users:
+                # Create a single user by providing an email (with a valid domain, Ex: @knowledgemd.com) and a password
+                -n <user>:<pass>, --new-email <user>:<pass>
+                                        
+                # Create multiple users based on the credentials on a file, format: <address>:<pass>
+                -N <creeds.txt>, --new-email-with-file <creeds.txt>
+                
+                # Create some number of valid credentials with random data, the credentials are formed the following way
+                # Address: Takes 3 random words and then the domain, <word>_<word>_<word>@<domain>.com
+                # Password: Takes one random word and then a random amount of alphanumeric characters
+                -R <x>, --random <x>  Create X account with random credentials
+
+            Delete users
+                [*] Both options support full address or partial addresses
+
+                # Delete account based on the address, must be logged in with '-l' or '-L' or credential being stored in cache
+                # This option also allows passing the index in cache to select an account, start from 0
+                -d <account id>, --delete <account id>
+
+                # Delete all accounts based on the addresses of a file
+                # *Index selection not supported
+                -D <creeds.txt>, --delete-with-file <creeds.txt>
+                                    
+
+        Account login arguments:
+            # Login into the account, this option can take as values: 
+                # cache index from 0 
+                # Address, if credentials are in cache
+                # <address>:<passwd>
+                # If we type 'cache' (without the collons), we will choose to loggin into all the accounts in cache
+            -l <user>:<pass>, --login <user>:<pass>
+
+            # Login into multiple accounts, should be combines with other arguments
+            # Accepts full/partinal credentials if they are in cache
+            # Dont accept cache index
+            -L <creeds.txt>, --login-with-file <creeds.txt>
+
+        Mail query arguments, must be logged in:
+            # Output the NEW (not marked as seen) emails of the logged-in account(s)
+            -i, --inbox
+            
+            # Output ALL the emails of the logged-in account(s)
+            -I, --inbox-all
+            
+            # Mark an email as seen from the specified account, we can use partial email_id (just the first characters)
+            # We can use this argument in 3 diferent ways
+                # Pass only the email_id: This will take the fist account from the logged in ones
+                # Pass an address and an email_id: This will look for the accout in cache to find te credentials, we can use partial emails
+                # Pass the address, password and email_id: This way doesnt need to have credentials in cache or use logged-in flags but will be slower since it will have to login with that credentials to retrieve a token
+            -m <addr>[:<pass>]:<email_id>, --mark <addr>[:<pass>]:<email_id>
+            
+            # Mark all emails as seen to all the logged-in accounts
+            -M, --mark-all
+
+        
+        """)
+
+
--- MailAPI_CLI.py
+++ MailAPI_CLI.py
@@ -1,7 +1,12 @@
+# Libraries
+from signal import signal, SIGINT
+from random import randint
+
+from os import path, rmdir
 from argparse import ArgumentParser
 from sys import exit
-from signal import signal, SIGINT
 
+# Tool modules
 from MailAPIClientClass import MailCLI
 from MailAPI import MailAPI
 # from MailAPIClientClass import *
@@ -14,36 +19,51 @@ def ctrl_c(sig, frame):
 signal(SIGINT, ctrl_c)
 
 
-# Argument setup
+# Argument parser setup
 parser = ArgumentParser(description="""This is the CLI from the API from -> https://api.mail.tm"""
                         )
+
+parser.add_argument("-H", "--help-advanced", action="store_true", help="Show extended help information")
+
 # Account create/delete arguments
 acc_cd = parser.add_argument_group("Account create & delete arguments")
-acc_cd.add_argument("-H", "--help-advanced", action="store_true", help="Show extended help information")
 acc_cd.add_argument("-n", "--new-email", metavar="<user>:<pass>", help="Type the user (not the @domain.com) and the password like this ")
 acc_cd.add_argument("-N", "--new-email-with-file", metavar="<creeds.txt>", help="Create multiple accounts taking creadential from a file")
 acc_cd.add_argument("-R", "--random", type=int, metavar='<x>', help="Create X account with random credentials")
-acc_cd.add_argument("-d", "--delete", metavar="<account id>", help="Delete account")
+acc_cd.add_argument("-d", "--delete", metavar="<address>", help="Delete account")
 acc_cd.add_argument("-D", "--delete-with-file", metavar="<creeds.txt>", help="Delete multiple accounts taking account IDs from a file")
 # Login
 acc_login = parser.add_argument_group("Account login arguments")
-acc_login.add_argument("-l", "--login", metavar="<user>:<pass>", help="Login into the account, this optian can take as values: cache index from 0 or <address>:<passwd>")
+acc_login.add_argument("-l", "--login", metavar="<user>:<pass>", help="Login into the account, this option can take as values: cache index from 0 or <address>:<passwd>")
 acc_login.add_argument("-L", "--login-with-file", metavar="<creeds.txt>", help="Login into multiple accounts, should be combines with other arguments, accepts full/partinal credentials if they are in cache, not index")
 # Get mails arguments
 acc_mails = parser.add_argument_group("Mail query arguments, must be logged in")
 acc_mails.add_argument("-i", "--inbox", action="store_true", help="Output all the emails of the logged-in account(s)")
 acc_mails.add_argument("-I", "--inbox-all", action="store_true", help="Output the new emails of the logged-in account(s)")
-acc_mails.add_argument("-m", "--mark", action="store_true", help="Mark all emails as seen to all the logged-in accounts")
+acc_mails.add_argument("-m", "--mark", metavar="<addr>[:<pass>]:<email_id>", help="Mark an email as seen from the specified account")
+acc_mails.add_argument("-M", "--mark-all", action="store_true", help="Mark all emails as seen to all the logged-in accounts")
 # Cache & output
-acc_cache = parser.add_argument_group("Output arguments")
-acc_cache.add_argument("-o", "--output", metavar="<creeds.txt>", help="Path where to save credentials stored in cache as <address>:<passwd>")
-acc_cache.add_argument("-q", "--quiet", default=False, action="store_true", help="The program wont output anything to the stdout")
+parser.add_argument("-o", "--output", metavar="<creeds.txt>", help="Path where to save credentials stored in cache as <address>:<passwd>")
+parser.add_argument("--disable-cache", default=False, action="store_true", help="Prevent the program to save cache data")
+parser.add_argument("--delete-cache", help="WARNING: This option will delete all the cached credential, this may affect the performance of the next requests")
+parser.add_argument("-q", "--quiet", default=False, action="store_true", help="The program wont output anything to the stdout")
 
 args = parser.parse_args()
 
 
 
 cli = MailCLI()
+
+# -------[ Configuration parameters ]--------
+if args.help_advanced:
+    cli.helpAdvanced()
+    exit()
+
+if args.disable_cache:
+    cli.disableCache = True
+
+if args.delete_cache:
+    rmdir(cli.cachePath)
 
 
 # -------[ Define actions ]--------
@@ -104,6 +124,18 @@ elif args.inbox_all:
     cli.inbox(showseen=True)
 
 if args.mark:
+    data = args.mark.split(":")
+    if len(data) == 1:      # Only id
+        cli.mark(data[0]) 
+    elif len(data) == 2:
+        acc = cli.findInCache(data[0])
+        cli.mark(eid=data[-1], acc=acc)
+    else: 
+        acc = MailAPI()
+        acc.login(email=data[0], password=data[1])
+        cli.mark(eid=data[2], acc=acc)
+
+elif args.mark_all:
     cli.mark()
 
 # -------[ Additional parameters ]-------- 
@@ -111,14 +143,3 @@ if args.output:
     cli.outputCache(args.output)
 
 
-if __name__ == '__main__':
-
-
-    # cli.newEmailAccount([["pruebas50@knowledgemd.com", "passwd"],["pruebas51@knowledgemd.com", "passwd"],["pruebas52@knowledgemd.com", "passwd"]])
-    # cli.deleteEmailAccount(cli.cacheAccounts)
-    
-    # cli.genRandomAccounts(2)
-
-    # cli.withFile("test.txt", "create")
-
-    pass
